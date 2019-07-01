f3cdbf8d117d3dd62d4994756b3586212a70380f
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-06-30 21:22:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+// 1
+class Breakfast {
+    constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+    }
+}
+
+// 2
+class Lunch {
+    constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+    }
+}
+
+// 3
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+    }
+}
